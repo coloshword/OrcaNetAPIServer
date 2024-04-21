79a310db807c3c0e26f6f1e44412950b2094b865
Acero Liang <acero.liangli@stonybrook.edu>
2024-04-21 12:41:37 -0400

    resetting git

---
+++ endpoints.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "net/http"
+    "github.com/coloshword/OrcaNetAPIServer/manageOrcaNet"
+    "strings"
+)
+// some basic endpoints
+
+// getRoot: the root endpoint ('/')
+func getRoot(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("got / request")
+    io.WriteString(w, "This is the root of the API server")
+}
+
+// getHello: the hello endpoint
+func getHello(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("got hello request")
+    io.WriteString(w, "Hello, HTTP!\n")
+}
+
+// getBlockchainInfo: endpoint to get the blockchain info
+func getBlockchainInfo(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("getBlockchainInfo request") 
+    const command string = "getblockchaininfo"
+    stdout, err :=  manageOrcaNet.CallBtcctlCmd(command)
+    fmt.Println(err)
+    // return the output of CallBtcctlCommand back to the querier 
+    io.WriteString(w, stdout)
+}
+
+// getNewAddress: endpoint to get a new wallet address
+// this wallet address can be used for mining rewards / sending / receiving transactions
+// For security purposes, it is recommended to create a new address everytime 
+func getNewAddress(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("getNewAddress request")
+    const command string = "getnewaddress --wallet"
+    stdout, err := manageOrcaNet.CallBtcctlCmd(command)
+    fmt.Println(err)
+    io.WriteString(w, stdout)
+}
+
+// getBalance: gets the balance of the wallet 
+func getBalance(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("getBalance endpoint")
+    const command string = "getbalance --wallet"
+    stdout, err := manageOrcaNet.CallBtcctlCmd(command)
+    fmt.Println(err)
+    io.WriteString(w, stdout)
+}
+
+// mine: endpoint to start mining, mining rewards go to the associated wallet on this node 
+func mine(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Mine endpoint")
+    // to mine, we need to restart the OrcaNet node with --generate and --mingaddr=<newAddress>
+    const getAddressCmd string = "getnewaddress --wallet"
+    stdout, err := manageOrcaNet.CallBtcctlCmd(getAddressCmd)
+    if err != nil {
+        fmt.Println("error getting a new address for mining")
+        io.WriteString(w, "error getting a new address for mining")
+    }
+    //std out is the address
+    address := strings.TrimSpace(stdout)
+    orcaNetParams := []string{"--generate", "--miningaddr=" + address}
+    fmt.Println("orcaParams[0] " + orcaNetParams[0])
+    fmt.Println("orcaParams[1] " + orcaNetParams[1])
+    if err := manageOrcaNet.Start(orcaNetParams...); err != nil {
+        fmt.Println("failed to start mining:", err)
+        http.Error(w, "failed to start mining", http.StatusInternalServerError)
+        return
+    }
+    io.WriteString(w, "Mining successfully started")
+}
+
+// stopMine: endpoint to stop mining
+func stopMine(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("stop mine endpoint")
+}
+
+
+
+
+
---
+++ manageOrcaNet/manager.go
@@ -0,0 +1,70 @@
+ package manageOrcaNet
+
+ import (
+     "fmt"
+     "os"
+     "os/exec"
+     "strings"
+ )
+
+ const (
+     orcaNetPath string = "../OrcaNet/OrcaNet"
+     btcctlPath string = "../OrcaNet/cmd/btcctl/btcctl"
+     orcaWalletPath string = "../OrcaWallet/btcwallet"
+ )
+ //startOrcaNet: starts the OrcaNet full node
+func Start(params ...string) error {
+    // check for the existence of executable
+
+    _, err := os.Stat(orcaNetPath)
+    if os.IsNotExist(err) {
+        fmt.Println("Cannot find OrcaNet executable")
+        return err
+    } 
+    // we know it exists 
+    cmd := exec.Command(orcaNetPath, params...)
+    fmt.Println(params)
+    if err := cmd.Start();  err != nil {
+        fmt.Println(err)
+        fmt.Println("failed to run")
+        return nil
+    }
+    fmt.Println("OrcaNet started successfully")
+    return nil
+}
+
+//startOrcaWallet: starts the OrcaWallet
+func StartOrcaWallet() error {
+    // check for the existence of the executable 
+    _, err := os.Stat(orcaWalletPath)
+    if os.IsNotExist(err) {
+        fmt.Println("Cannot find Orcawallet executable")
+        return err
+    }
+
+    cmd := exec.Command(orcaWalletPath)
+    if err := cmd.Start(); err != nil {
+        fmt.Println(err)
+        fmt.Println("failed to start wallet executable")
+        return nil
+    }
+    fmt.Println("Wallet started successfully")
+    return nil
+}
+
+// callBtcctlCmd: calls a Btcctl command Exactly as specified in string param, and returns the stdout of btcctl as a string 
+// its a singular string, but you can pass as many arguments, we will split the arguments in this fn
+func CallBtcctlCmd(cmdStr string) (string, error) {
+    params :=  strings.Split(cmdStr, " ") 
+    fmt.Println(params)
+    cmd := exec.Command(btcctlPath, params...)
+    // get the stdout of cmd, CAN HANG but shouldn't be a problem in a btcctl command
+    stdout, err := cmd.CombinedOutput() 
+    if err != nil {
+        return "", fmt.Errorf("failed to execute btcctl commands '%s': %s, error: %v", cmdStr, stdout, err)
+    }
+    fmt.Println(err);
+    return string(stdout), nil
+}
+
+
